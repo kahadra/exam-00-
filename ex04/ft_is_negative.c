a73209434eb932fb054f3fa06955a045b58eef60
Chunwoong Park <chpark@c1r1s2.42seoul.kr>
2021-09-12 16:13:57 +0900

    3

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chpark <chpark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/12 15:58:27 by chpark            #+#    #+#             */
+/*   Updated: 2021/09/12 16:12:27 by chpark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	negative;
+	char	positive;
+
+	negative = 'N';
+	positive = 'P';
+	if (n < 0)
+	{
+		write(1, &negative, 1);
+	}
+	else
+	{	
+		write(1, &positive, 1);
+	}
+}
